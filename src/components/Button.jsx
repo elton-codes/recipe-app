bc70fc9332a76aba2e3a85c4ef356141b4dad8a7
elton asamoah <eltonasamoah007@gmail.com>
2024-06-27 09:28:25 +0000

    React Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+export default function Button({title}) {
+    return (
+        <button className="block bg-lime-600">{title}</button>
+    );
+}
+
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+import welcomeImage from "../assets/images/welcome-image.png";
+
+export default function Welcome() {
+    return (
+        <div>
+            <h3>Savory & Sweet</h3>
+            <img src={welcomeImage} alt="Welcome image" />
+        </div>
+    )
+}
